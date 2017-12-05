622611caed26c47f3d0f55474f7257371eb8e638
Kyle Gerard Felker <kfelker@princeton.edu>
2017-12-05 11:27:52 -0500

    Add brute force traversal of graph for sol 1

---
+++ day5/jump_maze.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import numpy as np
+
+jumps = np.loadtxt('input.txt', dtype=np.int)
+njump = 0
+current_index = 0
+num_instructions = jumps.shape[0]
+
+while (current_index >= 0 and current_index < num_instructions):
+    jump_value = jumps[current_index]
+    jumps[current_index] = jump_value + 1
+    current_index = current_index + jump_value
+    njump = njump + 1
+print(njump)
