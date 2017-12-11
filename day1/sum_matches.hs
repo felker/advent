f65cd8d3aec9c9df229c00577267cbc6ac76bd35
Kyle Gerard Felker <kfelker@princeton.edu>
2017-12-11 17:22:34 -0500

    Add sol to part 1 of day1

--- day1/sum_matches.hs
+++ day1/sum_matches.hs
@@ -5,10 +5,22 @@ main = do
         contents <- readFile "input.txt"
         --print contents
         --print . lines $ contents -- Use Haskell's lines function to strip \n
-        -- But lines() now encloses the string in a list
+        -- But lines() now encloses the string in a list, i.e.
+        -- [["l", "i", "n", "e"]]
         let line1 = words contents
         --print (line1 !! 0)
         --print . map readInt . map (\c -> [c]) $ (line1 !! 0)
+        let line1_int = map readInt . map (\c -> [c]) $ (line1 !! 0)
+        print . sum_matches 1 $ line1_int
+        
 -- alternately, main = print . map readInt . words =<< readFile "test.txt"
 readInt :: String -> Int
 readInt = read
+
+-- can't use foldl directly, since the modified sum stencil depends on 2 points
+-- in list
+sum' :: (Num a) => [a] -> a  
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+-- define a new simple function based on zipWith
+sum_matches ncompare xs = sum $ zipWith (\val1 val2 -> if val1 == val2 then val1 else 0) xs (drop ncompare $ cycle xs)
